81619540dd978128fc8e618b4b3343d34849305f
Sojobo <aaron.otaku@gmail.com>
2019-03-30 11:21:25 +0000

    Removed red zones on weed and reduced price

--- resources/[esx]/esx_drugs/config.lua
+++ resources/[esx]/esx_drugs/config.lua
@@ -8,7 +8,7 @@ Config.Delays = {
 
 Config.DrugDealerItems = {
     GiveBlack = true, -- give black money? if disabled it'll give regular cash.
-	marijuana = 72
+	marijuana = 62
 }
 Config.WineDealerItems = {
     GiveBlack = false, -- give black money? if disabled it'll give regular cash.
@@ -22,9 +22,9 @@ Config.LicensePrices = {
 }
 
 Config.CircleZones = {
-	WeedField = {coords = vector3(310.91, 4290.87, 45.15), name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 100.0},
-	WeedProcessing = {coords = vector3(2329.02, 2571.29, 46.68), name = _U('blip_weedprocessing'), color = 25, sprite = 496, radius = 100.0},
-	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66), name = _U('blip_drugdealer'), color = 6, sprite = 500, radius = 50.0},
+	WeedField = {coords = vector3(310.91, 4290.87, 45.15), name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 0.0},
+	WeedProcessing = {coords = vector3(2329.02, 2571.29, 46.68), name = _U('blip_weedprocessing'), color = 25, sprite = 496, radius = 0.0},
+	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66), name = _U('blip_drugdealer'), color = 6, sprite = 500, radius = 0.0},
 
 	WineField = {coords = vector3(-1875.65, 2136.15, 126.71), name = "Marlowe Wine Fields", color = 27, sprite = 496, radius = 0.0},
 	WineProcessing = {coords = vector3(-1956.22, 1776.29, 183.06), name = "Wine Processing", color = 27, sprite = 496, radius = 0.0},
