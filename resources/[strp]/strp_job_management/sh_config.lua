6f60d6fdaee3281c4341f04ec0333d09b31d82a9
Sojobo <aaron.otaku@gmail.com>
2019-04-12 22:24:18 +0100

    hiring for rednecks

--- resources/[strp]/strp_job_management/sh_config.lua
+++ resources/[strp]/strp_job_management/sh_config.lua
@@ -2,6 +2,7 @@ JMConfig = {
   ["ambulance"] = { gradeRestriction = 2 },
   ["police"] = { gradeRestriction = 6 },
   ["lostmc"] = { gradeRestriction = 5 },
+  ["rednecks"] = { gradeRestriction = 4 },
   ["triads"] = { gradeRestriction = 5 },
-  ["vagos"] = { gradeRestriction = 5 }
+  ["vagos"] = { gradeRestriction = 5 },
 }
