ad972a53e42b54e474904c0d5ee33d57d0e8cef6
Sojobo <aaron.otaku@gmail.com>
2019-04-24 23:05:09 +0100

    Fix onPlayerKilled

--- resources/[system]/baseevents/server.lua
+++ resources/[system]/baseevents/server.lua
@@ -18,12 +18,12 @@ AddEventHandler('baseevents:onPlayerKilled', function(killedBy, data)
 		Citizen.Wait(0)
 	end
 
-    local killerPlayer = ESX.GetPlayerFromId(killedBy)
-    local victimPlayer = ESX.GetPlayerFromId(victim)
+    if (killedBy > -1) then
+        local killerPlayer = ESX.GetPlayerFromId(killedBy)
+        TriggerClientEvent('chatMessage', victim, 'SYSTEM', { 0, 0, 0 }, 'You were killed by ' .. killerPlayer.name)
+    end
 
-    -- local killString = "[" .. victimId .. "] " .. victimName .. " was killed by [" .. killerId .. "] " .. killerName
-    --TriggerClientEvent('chatMessage', -1, 'SYSTEM', { 0, 0, 0 }, victimName .. ' was just killed by ' .. killerName)
-    TriggerClientEvent('chatMessage', victim, 'SYSTEM', { 0, 0, 0 }, 'You were killed by ' .. killerPlayer.name)
+    local victimPlayer = ESX.GetPlayerFromId(victim)
     TriggerClientEvent('chatMessage', killedBy, 'SYSTEM', { 0, 0, 0 }, 'You just killed ' .. victimPlayer.name)
     -- PerformHttpRequest(GetConvar('discord_endpoint', ''), function(err, text, headers) end, 'POST', json.encode({username = "Kill Watchdog", content = killString }), { ['Content-Type'] = 'application/json' })
 
