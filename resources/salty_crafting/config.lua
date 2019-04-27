f0394f3f479f89d8e628574df52ded278a039f80
Frankie <rowanboot@hotmail.com>
2019-04-28 00:41:22 +0100

    Weapon/Item crafting

--- resources/salty_crafting/config.lua
+++ resources/salty_crafting/config.lua
@@ -1,26 +1,114 @@
 Config = {}
 
 -- Ammo given by default to crafted weapons
-Config.WeaponAmmo = 42
+Config.WeaponAmmo = 140
 
 Config.Recipes = {
 	-- Can be a normal ESX item
-	["lockpick"] = { 
-		{item = "bobbypin", quantity = 4 }, 
-		{item = "rubberband", quantity = 1 },
+	["WEAPON_PETROLCAN"] = { 
+		{item = "metal", quantity = 10 },
+		{item = "cloth", quantity = 5 }, 
+	},
+
+	["bandage"] = { 
+		{item = "cloth", quantity = 1 }, 
 	},
 	
-	-- Can be a weapon, must follow this format
-	['WEAPON_ASSAULTRIFLE'] = { 
-		{item = "steel", quantity = 4 }, 
-		{item = "gunpowder", quantity = 2},
-	}
+	["lockpick"] = { 
+		{item = "bobbypin", quantity = 3 }, 
+		{item = "rubberband", quantity = 1 },
+	},
+
+	-- 'Weapon parts'
+	["trigger"] = { 
+		{item = "metal", quantity = 1 }, 
+		{item = "spring", quantity = 1 },
+	},
+
+	["semiautotrigger"] = { 
+		{item = "metal", quantity = 3 }, 
+		{item = "spring", quantity = 3 },
+	},
+
+	["autotrigger"] = { 
+		{item = "metal", quantity = 5 }, 
+		{item = "spring", quantity = 5 },
+	},
+
+	["pistolbody"] = { 
+		{item = "metal", quantity = 2 }, 
+	},
+	
+	["semiautobody"] = { 
+		{item = "metal", quantity = 5 },
+		{item = "spring", quantity = 2 }, 
+	},
+
+	["smgbody"] = { 
+		{item = "metal", quantity = 10 },
+		{item = "spring", quantity = 5 }, 
+	},
+
+	-- melee
+	['WEAPON_BAT'] = { 
+		{item = "metal", quantity = 2 }, 
+		{item = "cloth", quantity = 1},
+	},
+	
+	['WEAPON_KNIFE'] = { 
+		{item = "metal", quantity = 1 }, 
+		{item = "cloth", quantity = 1},
+	},
+
+	['WEAPON_POOLCUE'] = { 
+		{item = "wood", quantity = 3 }, 
+	},
+
+	['WEAPON_BOTTLE'] = { 
+		{item = "brokenbottle", quantity = 1 },
+		{item = "cloth", quantity = 1 }, 
+	},
+
+--guns
+	['WEAPON_PISTOL'] = { 
+		{item = "metal", quantity = 2 }, 
+		{item = "cloth", quantity = 2},
+		{item = "metalpipe", quantity = 1},
+		{item = "trigger", quantity = 1},
+
+	},
+
+	['WEAPON_VINTAGEPISTOL'] = { 
+		{item = "metal", quantity = 3}, 
+		{item = "cloth", quantity = 3},
+		{item = "metalpipe", quantity = 1},
+		{item = "trigger", quantity = 1},
+		{item = "pistolbody", quantity = 1},
+	},
+
+	['WEAPON_MACHINEPISTOL'] = { 
+		{item = "metal", quantity = 5}, 
+		{item = "cloth", quantity = 5},
+		{item = "metalpipe", quantity = 1},
+		{item = "semiautotrigger", quantity = 1},
+		{item = "semiautobody", quantity = 1},
+	},
+
+	['WEAPON_SMG'] = { 
+		{item = "metal", quantity = 5}, 
+		{item = "cloth", quantity = 5},
+		{item = "metalpipe", quantity = 2},
+		{item = "autotrigger", quantity = 1},
+		{item = "smgbody", quantity = 1},
+	},
+
+	
 }
 
 -- Enable a shop to access the crafting menu
 Config.Shop = {
 	useShop = true,
-	shopCoordinates = { x=962.5, y=-1585.5, z=29.6 },
+	shopCoordinates = { x=1208.17, y=-3113.13, z=4.60},
 	shopName = "Crafting Station",
 	shopBlipID = 446,
 	zoneSize = { x = 2.5, y = 2.5, z = 1.5 },
---
+++ resources/salty_crafting/saltycraftingitems.sql
@@ -0,0 +1,15 @@
+INSERT INTO `items` (name, label) VALUES 
+	('cloth', 'Cloth'),
+	('bobbypin', 'Bobby Pin'),
+	('rubberband', 'Rubber Band'),
+	('metal', 'Metal'),
+	('spring', 'Spring'),
+	('wood', 'Wood'),
+	('metalpipe', 'Metal Pipe'),
+	('trigger', 'Trigger'),
+	('pistolbody', 'Pistol Body'),
+	('semiautotrigger', 'Semi Auto Trigger'),
+	('semiautobody', 'Semi Auto Body'),
+	('smgbody', 'SMG Body'),
+	('autotrigger', 'Auto Trigger')
+;
