27b3c638afcd2f34c413c2acdb968789a7ec0dbc
Frankie <rowanboot@hotmail.com>
2019-05-01 21:53:05 +0100

    tweaks

--- resources/gamz-skillsystem/config.lua
+++ resources/gamz-skillsystem/config.lua
@@ -7,13 +7,13 @@ Config.Notifications = true -- notification when skill is added
 Config.Skills = {
     ["Stamina"] = {
         ["Current"] = 0, -- Default value 
-        ["RemoveAmount"] = -0.4, -- % to remove when updating,
+        ["RemoveAmount"] = -0.5, -- % to remove when updating,
         ["Stat"] = "MP0_STAMINA" -- GTA stat hashname
     },
 
     ["Strength"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.4,
+        ["RemoveAmount"] = -0.1,
         ["Stat"] = "MP0_STRENGTH"
     },
 
@@ -25,24 +25,24 @@ Config.Skills = {
 
     ["Shooting"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.2,
+        ["RemoveAmount"] = -0.1,
         ["Stat"] = "MP0_SHOOTING_ABILITY"
     },
 
-    ["Driving"] = {
-        ["Current"] = 0,
-        ["RemoveAmount"] = -0.2,            
-        ["Stat"] = "MP0_WHEELIE_ABILITY"   ----   
+    -- ["Driving"] = {
+    --     ["Current"] = 0,
+    --     ["RemoveAmount"] = -0.2,            
+    --     ["Stat"] = "MP0_WHEELIE_ABILITY"    
                                                     
-    },
+    -- },
 
     ["Wheelie"] = {
         ["Current"] = 0,
         ["RemoveAmount"] = -0.2,        
-        ["Stat"] = "MP0_WHEELIE_ABILITY" ----  ---actually removes
-                                                --0.4 because 
-                                                        --its taking 
-                                                        --away from the same 
-                                                        --stat twice 
+        ["Stat"] = "MP0_WHEELIE_ABILITY" 
+                                                
+                                                        
+                                                    
+                                                        
     }
 }
