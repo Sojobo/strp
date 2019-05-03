72a8e3512c97b9929d13f785f7b6352be3a02adc
Frankie <rowanboot@hotmail.com>
2019-05-03 20:32:59 +0100

    Jewellery Heist

---
+++ resources/[esx]/esx_vangelico_robbery/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.RequiredCopsRob = 4
+Config.RequiredCopsSell = 0
+
+Stores = {
+	["jewelry"] = {
+		position = { ['x'] = -629.99, ['y'] = -236.542, ['z'] = 38.05 },       
+		-- reward = math.random(3000,15000),
+		nameofstore = "jewelry",
+		lastrobbed = 0
+	}
+}
