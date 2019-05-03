49580c6034a90c7c9a35ca9e2b018ea0bf606bac
Frankie <rowanboot@hotmail.com>
2019-05-03 14:30:29 +0100

    fix marker

--- resources/[esx]/esx_impound/config.lua
+++ resources/[esx]/esx_impound/config.lua
@@ -40,19 +40,19 @@ Config.ElapsedTimeBeforeRelease = 0
 
 Config.ImpoundLots = {
 	lot = {
-		Pos = {x=402.31, y= -1631.62, z= 29.29},
+		Pos = {x=402.31, y= -1631.62, z= 28.50},
 		Size  = {x = 3.0, y = 3.0, z = 1.0},
 		Color = {r = 204, g = 204, b = 0},
 		Marker = -1,
 		
 		DropoffPoint = {
-			Pos = {x=402.31, y= -1631.62, z= 29.29},
+			Pos = {x=402.31, y= -1631.62, z= 28.50},
 			Color = {r=58,g=100,b=122},
 			Size  = {x = 3.0, y = 3.0, z = 1.0},
 			Marker = 27
 		},
 		RetrievePoint = {
-			Pos = {x=408.04, y= -1638.57, z= 29.29},
+			Pos = {x=408.04, y= -1638.57, z= 28.50},
 			Color = {r=0,g=0,b=0},
 			Size  = {x = 3.0, y = 3.0, z = 1.0},
 			Marker = -1
