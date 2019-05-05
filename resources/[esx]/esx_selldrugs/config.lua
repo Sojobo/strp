b8c84d83a2cb5977832f7b9a8044d6e7709852ac
Frankie <rowanboot@hotmail.com>
2019-05-05 18:41:01 +0100

    needs to be worth the risk

--- resources/[esx]/esx_selldrugs/config.lua
+++ resources/[esx]/esx_selldrugs/config.lua
@@ -12,9 +12,9 @@ Config.SellWeed = true	-- if true, players can sell weed
 Config.SellMeth = false	-- if true, players can sell meth
 Config.SellCoke = true	-- if true, players can sell coke
 Config.SellOpiu = false	-- if true, players can sell opium
-Config.WeedPrice = 95	-- sell price for single, not pooch (black money)
+Config.WeedPrice = 110	-- sell price for single, not pooch (black money)
 Config.MethPrice = 250	-- sell price for single, not pooch (black money)
-Config.CokePrice = 130	-- sell price for single, not pooch (black money)
+Config.CokePrice = 200	-- sell price for single, not pooch (black money)
 Config.OpiuPrice = 300	-- sell price for single, not pooch (black money)
 Config.DistanceFromCity = 3000 -- set distance that player cant sell drugs too far from city
 Config.CityPoint = {x= -255.94, y= -983.93, z= 30.21}
